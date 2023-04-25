26f1cfe0a59d774ec7dcabb4d917e58eebbd6657
yann <yanndubois4@gmail.com>
2023-04-25 18:07:35 +0200

    made it working again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,8 @@
 import "@/styles/index.css";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "@material-tailwind/react";
-import { useEffect, useRef } from "react";
-import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider>
-      <Component {...pageProps} />;
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
-
-const usePreviousRoute = () => {
-  const { asPath } = useRouter();
-
-  const ref = useRef<string | null>(null);
-
-  useEffect(() => {
-    ref.current = asPath;
-  }, [asPath]);
-
-  return ref.current;
-};
-
-export { usePreviousRoute };
